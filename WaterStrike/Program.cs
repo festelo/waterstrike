aa73b5e0d2443b77e0803d5a3bd63e3ff0fbdf8e
festelo <mail@festelo.tk>
2018-07-04 01:01:21 +0300

    apikey and groupid now in env

--- WaterStrike/Program.cs
+++ WaterStrike/Program.cs
@@ -9,8 +9,9 @@ namespace WaterStrike
         static void Main(string[] args)
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            var controller = new VkController(149727984,
-                "40b9fe6b0e9c42fe25da42cf4c92b697b2c2917d03e7ba6dabf6a0dac4e827856938e4a477fb32a3b83fe");
+            var group = ulong.Parse(Environment.GetEnvironmentVariable("group"));
+            var key = Environment.GetEnvironmentVariable("key");
+            var controller = new VkController(group, key);
             controller.StartLongPoll().Wait();
             while (true)
             {
